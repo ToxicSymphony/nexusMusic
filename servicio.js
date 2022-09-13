91f9eab393e35050c00b6f2dcfabc61b2edcc83a
Toxic Symphony <brayan_stiv96@hotmail.com>
2022-09-13 17:36:14 -0500

    se comienza a consumir el api

---
+++ servicio.js
@@ -0,0 +1,25 @@
+const URI ="https://api.spotify.com/v1/artists/4tZwfgrHOc3mvqYlEYSvVi/top-tracks?market=US"
+console.log("hola")
+const TOKEN = "Bearer BQD53Slt2Gh5FYyrWxiZVyiQ1Hg5JU_DrNN0WujLtEA7mc9eEqzrFx4QHkKn20Ay8GJv6XNm0UvxN6Wz2GXirEih8NpBVEMJZmg3EVhKIoViVUtPG5CD8LDHj_vmZ9kf9fHmB9UOG6z_Qjimv5-fHMop1WdQIokc0xGU0sJJPn_ZFA"
+
+const PETICION = {
+    method:"GET",
+    headers: {
+        Authorization: TOKEN
+    }
+
+
+}
+
+fetch(URI,PETICION)
+.then(function(respuesta){
+    return(respuesta.json())
+
+})
+.then(function(respuesta){
+    console.log(respuesta)
+})
+.catch(function(respuesta){
+
+
+})
